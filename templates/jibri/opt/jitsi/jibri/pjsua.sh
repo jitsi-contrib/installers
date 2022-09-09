03c17550bda358983c6642f62e52641f755b2c8e
emrah <emrah@emrah.com>
2022-09-09 12:14:29 +0300

    jitsi-videosipgw, refactor: custom pjsua.sh

---
+++ templates/jibri/opt/jitsi/jibri/pjsua.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# kill old ffmpeg processes if exist
+pkill -U jibri -f ffmpeg
+sleep 0.2
+
+# push desktop views to virtual cameras
+ffmpeg -f x11grab -r 30 -i :0.0 -pix_fmt yuv420p -f v4l2 /dev/video1 &
+ffmpeg -f x11grab -r 30 -i :1.0 -pix_fmt yuv420p -f v4l2 /dev/video0 &
+sleep 0.8
+
+# use only parameter $2 which is the remote SIP address
+exec /usr/local/bin/pjsua --config-file /etc/jitsi/jibri/pjsua.config "$2" > /dev/null
