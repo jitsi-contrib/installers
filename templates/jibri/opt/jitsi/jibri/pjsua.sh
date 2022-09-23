45402bb1f97209a99634b2a3114707d633eb6495
emrah <emrah@emrah.com>
2022-09-23 15:43:14 +0300

    jitsi-videosipgw, fix: start ffmpeg on display :1 before google-chrome

--- templates/jibri/opt/jitsi/jibri/pjsua.sh
+++ templates/jibri/opt/jitsi/jibri/pjsua.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -e
 
-# kill old ffmpeg processes if exist
-pkill -U jibri -f ffmpeg || true
-sleep 0.2
-
-# push desktop views to virtual cameras
+# push display :0 view to virtual camera 1
 ffmpeg -f x11grab -r 30 -i :0.0 -pix_fmt yuv420p -f v4l2 /dev/video1 &
-ffmpeg -f x11grab -r 30 -i :1.0 -pix_fmt yuv420p -f v4l2 /dev/video0 &
 sleep 0.8
 
 # use only parameter $2 which is the remote SIP address
