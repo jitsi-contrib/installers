35a2a450c415f57727b42586635c33ee67b3dd05
emrah <emrah.eryilmaz@nordeck.net>
2023-05-03 15:48:01 +0300

    jitsi-videosipgw, refactor: update pjsua.sh according to new jibri

--- templates/jibri/opt/jitsi/jibri/pjsua.sh
+++ templates/jibri/opt/jitsi/jibri/pjsua.sh
@@ -5,5 +5,16 @@ set -e
 ffmpeg -f x11grab -r 30 -i :0.0 -pix_fmt yuv420p -f v4l2 /dev/video1 &
 sleep 0.8
 
-# use only parameter $2 which is the remote SIP address
-exec /usr/local/bin/pjsua --config-file /etc/jitsi/jibri/pjsua.config "$2" > /dev/null
+REGISTRAR=$(echo "$@" | egrep -o "..registrar=sip:[^ ]" || true)
+AUTO_ANSWER=$(echo "$@" | \
+    egrep -o "..auto-answer-timer=[0-9]+ ..auto-answer=[0-9]+" || true)
+
+# if auto-answer is set but there is no registrar, then this should be a direct
+# incoming call. Use the customized command in this case.
+if [[ -z "$REGISTRAR" && -n "$AUTO_ANSWER" ]]; then
+    exec /usr/local/bin/pjsua --config-file /etc/jitsi/jibri/pjsua.config \
+        "$AUTO_ANSWER" >/dev/null
+else
+    exec /usr/local/bin/pjsua --config-file /etc/jitsi/jibri/pjsua.config \
+        "$@" >/dev/null
+fi
