18cc6488271b4bf510082a37cad9bd9634302b94
emrah <emrah.eryilmaz@nordeck.net>
2024-02-16 16:28:51 +0300

    jitsi-videosipgw, fix: auto answer mode

--- templates/jibri/opt/jitsi/jibri/pjsua.sh
+++ templates/jibri/opt/jitsi/jibri/pjsua.sh
@@ -6,14 +6,15 @@ sudo systemctl start virtual-camera-1
 sleep 0.8
 
 REGISTRAR=$(echo "$@" | egrep -o "..registrar=sip:[^ ]" || true)
-AUTO_ANSWER=$(echo "$@" | \
-    egrep -o "..auto-answer-timer=[0-9]+ ..auto-answer=[0-9]+" || true)
+AUTO_ANSWER=$(echo "$@" | egrep -o "..auto-answer-timer=[0-9]+" || true)
 
 # if auto-answer is set but there is no registrar, then this should be a direct
 # incoming call. Use the customized command in this case.
 if [[ -z "$REGISTRAR" && -n "$AUTO_ANSWER" ]]; then
+    TIMER=$(echo $AUTO_ANSWER | cut -d '=' -f2)
+
     exec /usr/local/bin/pjsua --config-file /etc/jitsi/jibri/pjsua.config \
-        "$AUTO_ANSWER" >/dev/null
+        --auto-answer-timer=$TIMER --auto-answer=200 >/dev/null
 else
     exec /usr/local/bin/pjsua --config-file /etc/jitsi/jibri/pjsua.config \
         "$@" >/dev/null
